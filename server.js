533d39499656bddc546bc15e098d916f82f98e5c
Roshan <roshanpokhrel013@gmail.com>
2023-04-30 23:20:23 +0545

    Created authentication token

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require ('express')
 const app = express()
 
@@ -16,17 +18,29 @@ const posts = [
 ]
 
 app.get("/posts", (req,res)=>{
-    res.json(posts)
+    res.json(posts.filter(post => posts.username === req.user.name)) 
 })
 
-app.post("/", (req,res)=>{
+app.post("/login", (req,res)=>{
     //authenticate user
 
     const username = req.body.username
     const user = {user: username}
 
-    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({accessToken:accessToken})
 })
+
+function authenticationToken(req,res,next){
+    const header = req.headers['authorization']
+    const token = header && header.split[' '][1]
+    if (token===null) return res.status(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
+        if (err) return res.status(403)
+        req.user = user
+        next()
+    })
+}
 
 app.listen(3000)

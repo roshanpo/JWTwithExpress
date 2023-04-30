d33a4d78bb826d62e7bf35cb01bdf938b6005913
Roshan <roshanpokhrel013@gmail.com>
2023-04-30 23:49:55 +0545

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const posts = [
 }
 ]
 
-app.get("/posts", (req,res)=>{
+app.get("/posts",authenticateToken, (req,res)=>{
     res.json(posts.filter(post => posts.username === req.user.name)) 
 })
 
@@ -31,9 +31,9 @@ app.post("/login", (req,res)=>{
     res.json({accessToken:accessToken})
 })
 
-function authenticationToken(req,res,next){
+function authenticateToken(req,res,next){
     const header = req.headers['authorization']
-    const token = header && header.split[' '][1]
+    const token = header && header.split(' ')[1]
     if (token===null) return res.status(401)
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
